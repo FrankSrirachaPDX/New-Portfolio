e231ac875ab063f60668265e6cbc8bf08d5ccca3
Ryan <werner.ryan18@gmail.com>
2018-12-27 09:34:18 -0800

    started Javascript responsiveness

--- js/portfolioJS.js
+++ js/portfolioJS.js
@@ -1,15 +1,12 @@
-function aboutMe(){
-
+function getAbout(){
+    document.getElementById("JSobj").innerHTML="This is where I'd talk about myself, all my good swag";
 }
-function aboutHTML(){
-
+function getHtmlCss(){
+    document.getElementById("JSobj").innerHTML="Solid foundation and understanding of HTML and CSS.";
 }
-function aboutCSS(){
-
+function getJS(){
+    document.getElementById("JSobj").innerHTML=("Solid foundation with Javascript and fluent understanding of Object Orientated Programming.");
 }
-function aboutJS(){
-
-}
-function aboutUX(){
-    
+function getUX(){
+    document.getElementById("JSobj").innerHTML="Talentful Eye for the minute design of proper UX design.";
 }
